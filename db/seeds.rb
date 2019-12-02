4545a5817ace17a135d7271e60002a6bf6e37393
example <example@example.com>
2019-12-02 09:40:11 +0000

    change about page text

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
-50.times do |n|
+20.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -20,7 +20,7 @@ end
 
 # マイクロポスト
 users = User.order(:created_at).take(6)
-50.times do
+20.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content, title: "sample", author: "sample") }
 end
@@ -28,7 +28,7 @@ end
 # リレーションシップ
 users = User.all
 user  = users.first
-following = users[2..50]
-followers = users[3..40]
+following = users[2..20]
+followers = users[3..20]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
