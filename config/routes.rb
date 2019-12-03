a414184e4a86acd090f1aee6ae572fa6e8fb50d2
example <example@example.com>
2019-12-03 13:34:52 +0000

    display: none; twitter, facebook

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
 
   root 'static_pages#home'
-  get    '/home', to: 'static_pages#home'
-  get    '/about', to: 'static_pages#about'
-  get    '/signup', to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    'home', to: 'static_pages#home'
+  get    'about', to: 'static_pages#about'
+  get    'signup', to: 'users#new'
+  get    'login',   to: 'sessions#new'
+  post   'login',   to: 'sessions#create'
+  delete 'logout',  to: 'sessions#destroy'
   resources :users do
     member do
       get :following, :followers, :mypage
