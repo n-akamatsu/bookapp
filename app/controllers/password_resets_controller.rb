e64939f3ae62e968b92b331a9f2fd5c9fec26977
example <example@example.com>
2019-12-04 08:45:04 +0000

    change password reset redirect url

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,7 +29,7 @@ class PasswordResetsController < ApplicationController
     elsif @user.update_attributes(user_params)
       log_in @user
       flash[:success] = "パスワードはリセットされました"
-      redirect_to @user
+      redirect_to root_url
     else
       render 'edit'
     end
